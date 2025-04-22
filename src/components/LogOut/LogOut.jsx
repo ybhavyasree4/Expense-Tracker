ac5a5d9cc48d17944c5b8de08c6097c48d682b71
Sony <bhavyasreey54@gmail.com>
2025-04-22 20:39:10 +0530

    Fix import path for LogOut.css

--- src/components/LogOut/LogOut.jsx
+++ src/components/LogOut/LogOut.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Logout.css";
+import "./LogOut.css";
 
 const Logout = () => {
   const navigate = useNavigate();
